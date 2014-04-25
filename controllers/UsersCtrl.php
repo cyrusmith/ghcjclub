edda2e2389b051c78e019b6f66838ea1640a5bcc
DroN <acidron@gmail.com>
2014-04-25 11:44:37 +0400

    * API: выдача пользователя

--- controllers/UsersCtrl.php
+++ controllers/UsersCtrl.php
@@ -1,16 +1,20 @@
 <?
 class UsersCtrl extends DController {
+	protected $model;
+
+	function __construct() {
+		$this->model = new UserModel();
+	}
 	function lists(){
 //        $limit = Registry::get('topdeep');
 //        $this->customize->conditions = "1 LIMIT 0,$limit";
-		return (new DModelsCollection('UserModel'))->load('true limit 0,50');
+		return (new DModelsCollection($this->model))->load('true limit 0,50');
 	}
 	function listsOnline(){
 		// todo выбирать юзеров онлайн
 		return (new DModelsCollection('UserModel'))->load('id IN (2, 4) limit 0,9');
 	}
 	function show($id) {
-		$model = DI::create('UserModel', ['loadWith' => $id]);
-		return $model;
+		return $this->model->load("id = $id");
 	}
 }
