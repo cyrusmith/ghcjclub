39792604fab031dbc4ae0ac9581aa10610b4852a
Evgeny Popov <e.popov@outlook.com>
2014-04-26 12:34:42 +0600

    + громкость на минишапке + директива громкости

--- views/js/directives/playerUI.js
+++ views/js/directives/playerUI.js
@@ -1,41 +1,7 @@
 angular.module('CjClubUserApp').directive('playerUi', function (jplayerInterface, playlist, radioUrls) {
 	'use strict';
-	return {
-		restrict: 'EA',
-		scope: true,
-		controller: 'PlayerCtrl', //TODO перенсти в директиву
-		templateUrl: function (el, attr) {
-			var tpl = attr.tpl ? attr.tpl : 'player.html';
-			return 'views/main/includes/' + tpl;
-		},
-		link: function (scope, elem) {
-			$.each($(elem).find('.sound_control_slider'), function (count, item) {
-				//console.log(count);
-				$(item).slider({
-					animate: true,
-					orientation: "vertical",
-					range: "min",
-					min: 0,
-					max: 100,
-					value: 100,
-					slide: function (event, ui) {
-						jplayerInterface.setVolume(ui.value / 100);
-						$.each($('.sound_control_slider:not(:eq(' + count + '))'), function (c, i) {
-							$(i).slider('value', ui.value);
-						});
-					}
-				});
-			});
-			scope.$watch(
-				function () {
-					return jplayerInterface.getVolume();
-				},
-				function (v) {
-					$.each($(elem).find('.sound_control_slider'), function (count, item) {
-						$(item).slider('value', v * 100);
-					});
-				}
-			);
+	var
+		link = function (scope) {
 
 			scope.playlist = playlist;
 
@@ -88,6 +54,16 @@ angular.module('CjClubUserApp').directive('playerUi', function (jplayerInterface
 					scope.playRadio();
 				}
 			};
-		}
+		};
+
+	return {
+		restrict: 'EA',
+		scope: true,
+		controller: 'PlayerCtrl', //TODO перенсти в директиву
+		templateUrl: function (el, attr) {
+			var tpl = attr.tpl ? attr.tpl : 'player.html';
+			return 'views/main/includes/' + tpl;
+		},
+		link: link
 	};
 });
