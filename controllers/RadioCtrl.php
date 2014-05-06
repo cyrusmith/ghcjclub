e130e9055bf6dbaeffff7b95ade08fe77252747c
DroN <acidron@gmail.com>
2014-05-06 09:53:55 +0400

    + выдача радиоинфы через tracksCtrl

--- controllers/RadioCtrl.php
+++ controllers/RadioCtrl.php
@@ -8,9 +8,10 @@ class RadioCtrl extends DController {
 
 	function getInfo($channel) {
 		$number = 1;
-		$query = "select T.* from cjclub_radio R, cjclub_tracks T  where played_time IS NOT NULL AND channel_id = $channel AND track_id != 0 AND R.track_id = T.id ORDER BY R.id DESC LIMIT 0,$number";
-		$results = $this->db->query($query, DB_SELECT_OBJS);
+		$query = "select R.track_id from cjclub_radio R where played_time IS NOT NULL AND channel_id = $channel AND track_id != 0 ORDER BY R.id DESC LIMIT 0,$number";
+		$results = $this->db->query($query, DB_SELECT_COL);
 		if ($results == null) return [];
-		return array_shift($results);
+		$tracksCtrl = new TracksCtrl();
+		return $tracksCtrl->listsFiltered(null, null, null, $results);
 	}
 }
